9581be5ad17395cd12fbe99a7dc3b8d999829c65
Taylor Simpson <tdsimpson23@gmail.com>
2020-01-30 23:42:48 -0500

    Adding the Input types and using the spead operator in resolvers

--- src/index.js
+++ src/index.js
@@ -72,9 +72,29 @@ const typeDefs = `
     }
 
     type Mutation {
-        createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-        createComment(text: String!, author: ID!, post: ID!): Comment!
+        createUser(data: CreateUserInput!): User!
+        deleteUser(id: ID!): User!
+        createPost(data: CreatePostInput!): Post!
+        createComment(data: CreateCommentInput!): Comment!
+    }
+
+    input CreateUserInput{
+        name: String!,
+        email: String!,
+        age: Int
+    }
+
+    input CreatePostInput{
+        title: String!,
+        body: String!,
+        published: Boolean,
+        author: ID!
+    }
+
+    input CreateCommentInput{
+        text: String!,
+        author: ID!,
+        post: ID!
     }
 
     type User {
@@ -147,7 +167,7 @@ const resolvers = {
     },
     Mutation: {
         createUser(parent, args, ctx, info) {
-            const emailTaken = users.some((user) => user.email === args.email)
+            const emailTaken = users.some((user) => user.email === args.data.email)
 
             if (emailTaken) {
                 throw new Error('Email taken')
@@ -155,17 +175,22 @@ const resolvers = {
 
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args.data
             }
 
             users.push(user)
 
             return user
         },
+        deleteUser(parent, args, ctx, info) {
+            const userIndex = users.findIndex((user) => user.id === args.id)
+
+            if (userIndex === -1) {
+                throw new Error('User not found')
+            }
+        },
         createPost(parent, args, ctx, info) {
-            const userExists = users.some((user) => user.id === args.author)
+            const userExists = users.some((user) => user.id === args.data.author)
 
             if (!userExists) {
                 throw new Error('User not found')
@@ -173,10 +198,7 @@ const resolvers = {
 
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args.data
             }
 
             posts.push(post)
@@ -184,8 +206,8 @@ const resolvers = {
             return post
         },
         createComment(parent, args, ctx, info) {
-            const userExists = users.some((user) => user.id === args.author)
-            const postExists = posts.some((post) => post.id === args.post && post.published)
+            const userExists = users.some((user) => user.id === args.data.author)
+            const postExists = posts.some((post) => post.id === args.data.post && post.published)
 
             if (!userExists || !postExists) {
                 throw new Error('Unable to find user and post')
@@ -193,9 +215,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args.data
             }
 
             comments.push(comment)
